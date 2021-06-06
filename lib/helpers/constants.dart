98959e7e365a94cbe0ff3b5a630879541d4a3459
raine bayawa <rainebayawa@gmail.com>
2021-06-06 09:23:57 +0800

    riverpod config
    Added initial setup for riverpod and flutterfire

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,3 @@
+import 'package:firebase_core/firebase_core.dart';
+
+final Future<FirebaseApp> initialization = Firebase.initializeApp();
---
+++ lib/screens/startup/startup_pagecontroller.dart
@@ -0,0 +1,55 @@
+import 'package:daily_sillimanian_beta/helpers/constants.dart';
+import 'package:daily_sillimanian_beta/screens/startup/startup_view.dart';
+import 'package:flutter/material.dart';
+
+class StartUpPageController extends StatelessWidget {
+  const StartUpPageController({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+       // Initialize FlutterFire:
+      future: initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          // TODO - Create Error widget screen
+          return Scaffold(
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text("Something went wrong")],
+            ),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          // TODO - Create switch condition
+          // check user auth if already authenticated before or not, use shareprefrencess to check previous user locally
+          //  switch (authProvider.status) {
+          //   case Uninitialized:
+          //     return Loading();
+          //   case Unauthenticated:
+          //   case Authenticating:
+          //     return SignInView();
+          //   case Authenticated:
+          //     return HomeVIew(); //screen for all list of events/activities....
+          //   default:
+          //     return StartUpVIew();
+          // }
+
+          return StartUpview(); // direct navigate here for now until further features
+        }
+
+        // Otherwise, show something is waiting for initialization to complete
+        return Scaffold(
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [CircularProgressIndicator()],
+          ),
+        );
+      },
+      
+    );
+  }
+}
