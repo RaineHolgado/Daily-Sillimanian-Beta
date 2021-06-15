c76c5adb9e44097b6624a635c5348f03a2049918
raine bayawa <rainebayawa@gmail.com>
2021-06-15 20:09:31 +0800

    up

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class DailySillimanBetaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Daily Sillimanian Beta',
       theme: ThemeData( // TODO - create custom Theme for the app
         primarySwatch: Colors.blue,
--- lib/screens/startup/startup_view.dart
+++ lib/screens/startup/startup_view.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
 
 class StartUpview extends StatelessWidget {
-  const StartUpview({ Key? key }) : super(key: key);
+  const StartUpview({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      body: Center(
-        child: Text("Start Up View"),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 49),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(backgroundColor: Colors.red),
+                  child: Text("Pressed"),
+                ),
+              ),
+              SizedBox(height: 10),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  color: Colors.red,
+                  height: 40,
+                  width: double.infinity,
+                  child: Text("Button"),
+                ),
+              ),
+              SizedBox(height: 10),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  color: Colors.red,
+                  height: 40,
+                  width: double.infinity,
+                  child: Text("Button"),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
