91d79607d5735d3e6b2574ccb082366ba65b2ad0
raine bayawa <rainebayawa@gmail.com>
2021-06-28 19:57:56 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:daily_sillimanian_beta/helpers/dsb_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:daily_sillimanian_beta/app/router.gr.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      // statusBarColor: Color(0xFF870510),
+      // systemNavigationBarColor: Color(0xFF870510),
+      statusBarColor: Colors.black,
+      systemNavigationBarColor: Colors.white,
+    ),
+  );
   runApp(ProviderScope(child: DailySillimanBetaApp()));
 }
 
