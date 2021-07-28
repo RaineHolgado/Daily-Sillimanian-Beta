cfa3c9b4ea94c5ce5e64a8bcde1886a2b00945fa
raine bayawa <rainebayawa@gmail.com>
2021-07-28 16:43:30 +0800

    update

--- lib/models/events_model.dart
+++ lib/models/events_model.dart
@@ -1,36 +1,83 @@
 import 'dart:convert';
-
+import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Events {
   String? id;
-  Timestamp? created_at;
+  String? id_Organizations;
+  String? id_Users;
+  Timestamp? date_Created;
+  Timestamp? date;
+  String? orgLogo;
+  String? orgName;
+  String? eventImage;
+  String? title;
   Events({
     this.id,
-    this.created_at,
+    this.id_Organizations,
+    this.id_Users,
+    this.date_Created,
+    this.date,
+    this.orgLogo,
+    this.orgName,
+    this.eventImage,
+    this.title,
   });
+
+  String? get toDate => this.date != null
+      ? DateFormat("MMMM dd, yyyy")
+          .format(DateTime.parse(this.date!.toDate().toString()))
+      : null;
 
   Events copyWith({
     String? id,
-    Timestamp? created_at,
+    String? id_Organizations,
+    String? id_Users,
+    Timestamp? date_Created,
+    Timestamp? date,
+    String? orgLogo,
+    String? orgName,
+    String? eventImage,
+    String? title,
   }) {
     return Events(
       id: id ?? this.id,
-      created_at: created_at ?? this.created_at,
+      id_Organizations: id_Organizations ?? this.id_Organizations,
+      id_Users: id_Users ?? this.id_Users,
+      date_Created: date_Created ?? this.date_Created,
+      date: date ?? this.date,
+      orgLogo: orgLogo ?? this.orgLogo,
+      orgName: orgName ?? this.orgName,
+      eventImage: eventImage ?? this.eventImage,
+      title: title ?? this.title,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'created_at': created_at,
+      'id_Organizations': id_Organizations,
+      'id_Users': id_Users,
+      'date_Created': date_Created,
+      'date': date,
+      'orgLogo': orgLogo,
+      'orgName': orgName,
+      'eventImage': eventImage,
+      'title': title,
     };
   }
 
   factory Events.fromMap(Map<String, dynamic>? map) {
     return Events(
       id: map?['id'],
-      created_at: map?['created_at'],
+      id_Organizations: map?['id_Organizations'],
+      id_Users: map?['id_Users'],
+      date_Created: map?['date_Created'],
+      date: map?['date'],
+      orgLogo: map?['orgLogo'],
+      orgName: map?['orgName'],
+      eventImage: map?['eventImage'],
+      title: map?['title'],
     );
   }
 
@@ -39,17 +86,36 @@ class Events {
   factory Events.fromJson(String source) => Events.fromMap(json.decode(source));
 
   @override
-  String toString() => 'Events(id: $id, created_at: $created_at)';
+  String toString() {
+    return 'Events(id: $id, id_Organizations: $id_Organizations, id_Users: $id_Users, date_Created: $date_Created, date: $date, orgLogo: $orgLogo, orgName: $orgName, eventImage: $eventImage, title: $title)';
+  }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-  
+
     return other is Events &&
-      other.id == id &&
-      other.created_at == created_at;
+        other.id == id &&
+        other.id_Organizations == id_Organizations &&
+        other.id_Users == id_Users &&
+        other.date_Created == date_Created &&
+        other.date == date &&
+        other.orgLogo == orgLogo &&
+        other.orgName == orgName &&
+        other.eventImage == eventImage &&
+        other.title == title;
   }
 
   @override
-  int get hashCode => id.hashCode ^ created_at.hashCode;
+  int get hashCode {
+    return id.hashCode ^
+        id_Organizations.hashCode ^
+        id_Users.hashCode ^
+        date_Created.hashCode ^
+        date.hashCode ^
+        orgLogo.hashCode ^
+        orgName.hashCode ^
+        eventImage.hashCode ^
+        title.hashCode;
+  }
 }
--- lib/screens/tab_navigation/home/dummy_events.dart
+++ lib/screens/tab_navigation/home/dummy_events.dart
@@ -1,38 +1,38 @@
-class DummyEvents {
-  final String title;
-  final String date;
-  final String organization;
-  final String orgLogo;
-  final String posterImage;
-  DummyEvents({
-    required this.title,
-    required this.date,
-    required this.organization,
-    required this.orgLogo,
-    required this.posterImage,
-  });
+// class DummyEvents {
+//   final String title;
+//   final String date;
+//   final String organization;
+//   final String orgLogo;
+//   final String posterImage;
+//   DummyEvents({
+//     required this.title,
+//     required this.date,
+//     required this.organization,
+//     required this.orgLogo,
+//     required this.posterImage,
+//   });
 
-  static List<DummyEvents> get items => [
-        DummyEvents(
-          title: "PHICES Freshman Orientation 2021",
-          date: "July 3, 2022",
-          organization: "phices",
-          orgLogo: "assets/images/org3.png",
-          posterImage: "assets/images/activity1.png",
-        ),
-        DummyEvents(
-          title: "Architect Design Competitions 2021",
-          date: "July 3, 2022",
-          organization: "arki",
-          orgLogo: "assets/images/org2.png",
-          posterImage: "assets/images/activity2.png",
-        ),
-        DummyEvents(
-          title: "Million Dollar Company Opening 2021",
-          date: "July 3, 2022",
-          organization: "pyritecrew",
-          orgLogo: "assets/images/org1.png",
-          posterImage: "assets/images/activity3.png",
-        ),
-      ];
-}
+//   static List<DummyEvents> get items => [
+//         DummyEvents(
+//           title: "PHICES Freshman Orientation 2021",
+//           date: "July 3, 2022",
+//           organization: "phices",
+//           orgLogo: "assets/images/org3.png",
+//           posterImage: "assets/images/activity1.png",
+//         ),
+//         DummyEvents(
+//           title: "Architect Design Competitions 2021",
+//           date: "July 3, 2022",
+//           organization: "arki",
+//           orgLogo: "assets/images/org2.png",
+//           posterImage: "assets/images/activity2.png",
+//         ),
+//         DummyEvents(
+//           title: "Million Dollar Company Opening 2021",
+//           date: "July 3, 2022",
+//           organization: "pyritecrew",
+//           orgLogo: "assets/images/org1.png",
+//           posterImage: "assets/images/activity3.png",
+//         ),
+//       ];
+// }
